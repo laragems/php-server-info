3e49f295c66e34415e3b545b727d898d1c0573ea
Marius Puiu <puiumarius@gmail.com>
2016-04-05 12:54:02 +0300

    API test

--- test.php
+++ test.php
@@ -3,12 +3,10 @@
 require "vendor/autoload.php";
 
 use Laragems\ServerInfo\ServerInfo;
-use Laragems\ServerInfo\OS\OSType;
 
 /** @var \Laragems\ServerInfo\Interfaces\OSInterface $os */
 $os = ServerInfo::getServer();
 
 var_dump("Name: {$os->getName()}");
 var_dump("Version: {$os->getVersion()}");
-$osType = $os->getType() === OSType::LINUX;
-var_dump("OS Type: {$osType}");
+var_dump("OS Type: {$os->getType()}");
